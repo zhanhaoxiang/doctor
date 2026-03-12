b62ddb4fcfdc7c1f4715e19ca6fddcda305acee6
zhanhaoxiang <zhanhaoxiang@gmail.com>
2026-03-12 09:40:12 +0800

    桌面模拟窗口

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -4,9 +4,15 @@ import FlutterMacOS
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController()
-    let windowFrame = self.frame
     self.contentViewController = flutterViewController
-    self.setFrame(windowFrame, display: true)
+
+    // 固定为 iPhone 375×812 尺寸，模拟 iOS 开发体验
+    let fixedSize = NSSize(width: 375, height: 812)
+    self.setContentSize(fixedSize)
+    self.minSize = fixedSize
+    self.maxSize = fixedSize
+    self.styleMask.remove(.resizable)
+    self.center()
 
     RegisterGeneratedPlugins(registry: flutterViewController)
 
